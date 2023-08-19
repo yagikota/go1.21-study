7a2541b885fbe6d47c5b712e2404c7273f54ddaf
Kota <mdryzk64smsh@gmail.com>
2023-08-19 21:55:40 +0900

    Contains/ContainsFunc

--- slices/main.go
+++ slices/main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	"slices"
 )
 
+// BinarySearch/BinarySearchFunc
+// func main() {
+
+// 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+// 	idx, ok := slices.BinarySearch(nums, 5)
+// 	fmt.Println(idx, ok)
+
+// 	data := []int{1, 10, 11, 2} // sorted lexicographically
+// 	cmp := func(a int, b string) int {
+// 		return strings.Compare(strconv.Itoa(a), b)
+// 	}
+// 	pos, found := slices.BinarySearchFunc(data, "2", cmp)
+// 	fmt.Println(pos, found)
+// }
+
 func main() {
-
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-
-	idx, ok := slices.BinarySearch(nums, 5)
-	fmt.Println(idx, ok)
-
-	data := []int{1, 10, 11, 2} // sorted lexicographically
-	cmp := func(a int, b string) int {
-		return strings.Compare(strconv.Itoa(a), b)
-	}
-	pos, found := slices.BinarySearchFunc(data, "2", cmp)
-	fmt.Println(pos, found)
+	numbers := []int{0, 42, -10, 8}
+	hasNegative := slices.Contains(numbers, -10)
+	fmt.Println("Has a negative:", hasNegative)
 }
