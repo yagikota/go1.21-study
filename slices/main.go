8eb5d8178b0cf12e7dca11ac8feb7f771f6da825
Kota <mdryzk64smsh@gmail.com>
2023-08-19 23:26:58 +0900

    s[:len(s):len(s)] vs s[:len(s)]

--- slices/main.go
+++ slices/main.go
@@ -30,10 +30,36 @@ import (
 // }
 
 // IndexFunc
+// func main() {
+// 	numbers := []int{0, 42, -10, 8}
+// 	i := slices.IndexFunc(numbers, func(n int) bool {
+// 		return n < 0
+// 	})
+// 	fmt.Println("First negative at index", i)
+// }
+
+// Clip
+// func main() {
+// 	s := make([]int, 0, 5)
+// 	fmt.Printf("slice: %v, cap:%v , len: %v \n", s, cap(s), len(s))
+// 	s = append(s, 0)
+// 	fmt.Printf("slice: %v, cap:%v , len: %v \n", s, cap(s), len(s))
+// 	s = slices.Clip(s)
+// 	fmt.Printf("%v, %v\n", s, s)
+// 	fmt.Printf("slice: %v, cap:%v , len: %v \n", s, cap(s), len(s))
+// }
+
+// https://go.dev/blog/slices-intro
+// s[:len(s):len(s)] vs s[:len(s)]
 func main() {
-	numbers := []int{0, 42, -10, 8}
-	i := slices.IndexFunc(numbers, func(n int) bool {
-		return n < 0
-	})
-	fmt.Println("First negative at index", i)
+	s1 := make([]int, 0, 5)
+	fmt.Printf("slice1: %v, cap:%v , len: %v \n", s1, cap(s1), len(s1))
+	s1 = s1[:3]
+	fmt.Printf("slice1: %v, cap:%v , len: %v \n", s1, cap(s1), len(s1))
+
+	s2 := make([]int, 0, 5)
+	fmt.Printf("slice2: %v, cap:%v , len: %v \n", s2, cap(s2), len(s2))
+	// s2 = s2[:len:cap]
+	s2 = s2[:3:3]
+	fmt.Printf("slice2: %v, cap:%v , len: %v \n", s2, cap(s2), len(s2))
 }
